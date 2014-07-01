c47df4ee0904f1513f61f857eeb9909038b7c069
ghohun <ghohun.mail@gmail.com>
2014-06-30 23:47:19 -0300

    Agregado de logout y reordenamiento de las funciones

--- view/inicio.php
+++ view/inicio.php
@@ -1,3 +1,7 @@
+<?php
+sec_session_start() 
+?>
+
 <html>
 <!- Es la base no tiene CSS ni nada todo lo del estilo deberia ir en un archivo .css
   y hay que cambiar el estilo es solo la maquetacion jajjajaaja
@@ -25,12 +29,26 @@
 <br>
 <br>
 <br>
+<?php 
+if (!login_check())
+{//Si no esta logueado, muestro formulario de login
+?>
 <form id="formulario" action="controller/verificar.php" method="post">
    usuario:<input name="user" id="input1"></><br>
  contrase&ntilde;a:<input type="password" name="pass" id="input2"></><br>
  <input name="login" type="submit" value="ingresar">
   
 </form>
+<?php
+}
+else
+{
+?>
+Ya Logueado (<a href="index.php?view=logout.php">Desloguearse</a>)
+<?php
+}
+
+?>
 <br>
 <br>
 <br>
